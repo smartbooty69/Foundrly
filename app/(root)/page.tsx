015165ea028e0f51316229815aeb34482b3b92c8
Clancy Mendonca <clancymendonca@gmail.com>
2025-06-19 00:30:37 +0530

    Author Authentication Flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupTypeCard } from "../../components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({searchParams}:{
   searchParams: Promise<{ query?:string }>
@@ -9,6 +10,8 @@ export default async function Home({searchParams}:{
 
   const  query  = await (await searchParams).query;
   const params = {search: query || null};
+  const session = await auth();
+  console.log(session?.id);
   const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
   return (
