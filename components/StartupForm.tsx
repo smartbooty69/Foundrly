fe1922a32cabf4a8af3003f2c3616428fe8d5ea5
Clancy Mendonca <clancymendonca@gmail.com>
2025-06-19 13:35:19 +0530

    Server Action

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -30,18 +30,18 @@ const StartupForm = () => {
 
       await formSchema.parseAsync(formValues);
 
-      // const result = await createPitch(prevState, formData, pitch);
+      const result = await createPitch(prevState, formData, pitch);
 
-      // if (result.status == "SUCCESS") {
-      //   toast({
-      //     title: "Success",
-      //     description: "Your startup pitch has been created successfully",
-      //   });
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully",
+        });
 
-      //   router.push(`/startup/${result._id}`);
-      // }
+        router.push(`/startup/${result._id}`);
+      }
 
-      // return result;
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErorrs = error.flatten().fieldErrors;
